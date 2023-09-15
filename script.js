546d83cc97571b388144bfbf6e013eafb4af7983
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-16 02:03:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function clock(){
+
+	let today = new Date();
+	
+	let minutes = today.getMinutes();
+	if(minutes<10)
+	{
+		minutes = "0"+minutes;
+	}
+
+	let seconds = today.getSeconds();
+	if(seconds<10){
+		seconds = "0"+seconds;
+	}
+
+	let time = document.getElementById("timer");
+	time.innerHTML = date();
+}
